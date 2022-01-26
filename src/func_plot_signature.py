dedf0b380ee01df8a5c00abe9e70025724852d79
bouaziz <laurene.bouaziz@deltares.nl>
2022-01-26 17:51:44 +0100

    update plot_results (still needs to be tested with snake)

--- src/func_plot_signature.py
+++ src/func_plot_signature.py
@@ -6,6 +6,7 @@ Created on Wed Jul 14 09:18:38 2021
 """
 
 import hydromt
+from hydromt.stats import skills
 import numpy as np
 import matplotlib.pyplot as plt
 import os
@@ -27,19 +28,19 @@ def plot_signatures(dsq, labels, colors, linestyles, markers, Folder_out, statio
     #perf metrics for single station 
     for label in labels:
         #nse
-        nse = hydromt.stats.nashsutcliffe(dsq['Q'].sel(runs=label), dsq['Q'].sel(runs='Obs.'))
+        nse = skills.nashsutcliffe(dsq['Q'].sel(runs=label), dsq['Q'].sel(runs='Obs.'))
         dsq['performance'].loc[dict(runs = label, metrics = 'NSE')] = nse
         #nse logq
-        nselog = hydromt.stats.lognashsutcliffe(dsq['Q'].sel(runs=label), dsq['Q'].sel(runs='Obs.'))
+        nselog = skills.lognashsutcliffe(dsq['Q'].sel(runs=label), dsq['Q'].sel(runs='Obs.'))
         dsq['performance'].loc[dict(runs = label, metrics = 'NSElog')] = nselog
         #kge
-        kge = hydromt.stats.kge(dsq['Q'].sel(runs=label), dsq['Q'].sel(runs='Obs.'))
+        kge = skills.kge(dsq['Q'].sel(runs=label), dsq['Q'].sel(runs='Obs.'))
         dsq['performance'].loc[dict(runs = label, metrics = 'KGE')] = kge['kge']
         #rmse
-        rmse = hydromt.stats.rmse(dsq['Q'].sel(runs=label), dsq['Q'].sel(runs='Obs.'))
+        rmse = skills.rmse(dsq['Q'].sel(runs=label), dsq['Q'].sel(runs='Obs.'))
         dsq['performance'].loc[dict(runs = label, metrics = 'RMSE')] = rmse
         #mse
-        mse = hydromt.stats.mse(dsq['Q'].sel(runs=label), dsq['Q'].sel(runs='Obs.'))
+        mse = skills.mse(dsq['Q'].sel(runs=label), dsq['Q'].sel(runs='Obs.'))
         dsq['performance'].loc[dict(runs = label, metrics = 'MSE')] = mse   
 #         print(nse.values, nselog.values, kge['kge'].values, rmse.values, mse.values)
     
@@ -49,7 +50,9 @@ def plot_signatures(dsq, labels, colors, linestyles, markers, Folder_out, statio
 #         df = dsq['performance'].sel(runs = label, metrics = ['NSE', 'NSElog', 'KGE']).to_dataframe()
 #         df_perf = pd.concat([df,df_perf])
     
-    fig, axes = plt.subplots(5,2, figsize=(16/2.54, 22/2.54))
+    # fig, axes = plt.subplots(5,2, figsize=(16/2.54, 22/2.54), tight_layout=True)
+    fig = plt.figure(figsize=(16/2.54, 22/2.54), tight_layout=True)
+    axes = fig.subplots(nrows = 5, ncols = 2)
     axes = axes.flatten()
 
     # daily against each other axes[0]
@@ -63,14 +66,15 @@ def plot_signatures(dsq, labels, colors, linestyles, markers, Folder_out, statio
     axes[0].set_xlabel('Observed Q (m$^3$s$^{-1}$)', fontsize = fs)
 #     axes[0].legend(frameon=True, fontsize = fs, )
     axes[0].legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left',
-           ncol=2, mode="expand", borderaxespad=0., fontsize = fs,)
+            ncol=2, mode="expand", borderaxespad=0., fontsize = fs,)
     
     #r2 
     text_label = "" 
     for label in labels:
         r2_score = rsquared(dsq['Q'].sel(runs = 'Obs.'), dsq['Q'].sel(runs = label))
         text_label = text_label + f"R$_2$ {label} = {r2_score:.2f} \n"
-    axes[0].text(max_y/2, max_y/8, text_label, fontsize=fs)
+    # axes[0].text(max_y/2, max_y/8, text_label, fontsize=fs)
+    axes[0].text(0.2, 0.7, text_label, transform = axes[0].transAxes, fontsize=fs)
     # axes[0].text(max_y/2, max_y/8, f"R$_2$ {label} = {r2_score:.2f} \nR$_2$ {label_01} = {r2_score_new:.2f} ", fontsize=fs)  
 
     
@@ -84,7 +88,7 @@ def plot_signatures(dsq, labels, colors, linestyles, markers, Folder_out, statio
     axes[1].set_title('')
     axes[1].set_xticks(np.arange(1,13))
     axes[1].legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left',
-           ncol=3, mode="expand", borderaxespad=0., fontsize = fs,)
+            ncol=3, mode="expand", borderaxespad=0., fontsize = fs,)
     
     #FDC axes[2]
     for label, color, linestyle in zip(labels, colors, linestyles):
@@ -114,7 +118,8 @@ def plot_signatures(dsq, labels, colors, linestyles, markers, Folder_out, statio
     for label in labels:
         r2_score = rsquared(dsq_max['Q'].sel(runs = 'Obs.'), dsq_max['Q'].sel(runs = label))
         text_label = text_label + f"R$_2$ {label} = {r2_score:.2f} \n"
-    axes[4].text(max_y/2, max_y/8, text_label, fontsize=fs)
+    # axes[4].text(max_y/2, max_y/8, text_label, fontsize=fs)
+    axes[4].text(0.5, 0.05, text_label, transform = axes[4].transAxes, fontsize=fs)
     # axes[4].text(max_y/2, max_y/8, f"R$_2$ {label} = {r2_score:.2f} \nR$_2$ {label_01} = {r2_score_new:.2f}", fontsize=fs)   
     #add MHQ
     mhq = dsq_max.mean('time')
@@ -138,8 +143,10 @@ def plot_signatures(dsq, labels, colors, linestyles, markers, Folder_out, statio
     for label in labels:
         r2_score = rsquared(dsq_nm7q['Q'].sel(runs = 'Obs.'), dsq_nm7q['Q'].sel(runs = label))
         text_label = text_label + f"R$_2$ {label} = {r2_score:.2f} \n"
-    axes[5].text(max_y/2, max_y/8, text_label, fontsize=fs)
-    # axes[5].text(max_ylow*1.1/2, max_ylow*1.1/8, f"R$_2$ {label} = {r2_score:.2f} \nR$_2$ {label_01} = {r2_score_new:.2f} ", fontsize=fs) 
+    # import pdb; pdb.set_trace()
+    # axes[5].text(max_ylow*1.1/2, max_ylow*1.1/8, text_label, fontsize=fs)
+    axes[5].text(0.5, 0.05, text_label, transform = axes[5].transAxes, fontsize=fs)
+    ## axes[5].text(max_ylow*1.1/2, max_ylow*1.1/8, f"R$_2$ {label} = {r2_score:.2f} \nR$_2$ {label_01} = {r2_score_new:.2f} ", fontsize=fs) 
     #labels
     axes[5].set_ylabel('Simulated NM7Q (m$^3$s$^{-1}$)', fontsize = fs)
     axes[5].set_xlabel('Observed NM7Q (m$^3$s$^{-1}$)', fontsize = fs)
@@ -214,8 +221,9 @@ def plot_signatures(dsq, labels, colors, linestyles, markers, Folder_out, statio
     for ax in axes:
         ax.tick_params(axis='both', labelsize = fs)
         ax.set_title('')
-    
-    plt.tight_layout()
+    # fig.set_tight_layout(True)
+    # plt.tight_layout()
+    # plt.subplots_adjust(wspace = 0.5, hspace = 0.6)
     plt.savefig(os.path.join(Folder_out, f"signatures_{station_name}.png"), dpi = 300)
     
     
@@ -224,17 +232,20 @@ def plot_hydro(dsq, start_long, end_long, year_1, year_2, labels, colors, Folder
     #long period
     for label, color in zip(labels, colors):
         dsq['Q'].sel(runs = label, time = slice(start_long, end_long)).plot(ax=axes[0], label = label, linewidth = lw, color = color)
-    dsq['Q'].sel(runs = 'Obs.', time = slice(start_long, end_long)).plot(ax=axes[0], label = 'Obs.', linewidth = lw, color = 'k', linestyle = '--')
+    if "Obs." in dsq["runs"]:
+        dsq['Q'].sel(runs = 'Obs.', time = slice(start_long, end_long)).plot(ax=axes[0], label = 'Obs.', linewidth = lw, color = 'k', linestyle = '--')
     
     #s1-e1
     for label, color in zip(labels, colors):
         dsq['Q'].sel(runs = label, time = year_1).plot(ax=axes[1], label = label, linewidth = lw, color = color)
-    dsq['Q'].sel(runs = 'Obs.', time = year_1).plot(ax=axes[1], label = 'Obs.', linewidth = lw, color = 'k', linestyle = '--')
+    if "Obs." in dsq["runs"]:
+        dsq['Q'].sel(runs = 'Obs.', time = year_1).plot(ax=axes[1], label = 'Obs.', linewidth = lw, color = 'k', linestyle = '--')
     
     #s2-e2
     for label, color in zip(labels, colors):
         dsq['Q'].sel(runs = label, time = year_2).plot(ax=axes[2], label = label, linewidth = lw, color = color)
-    dsq['Q'].sel(runs = 'Obs.', time = year_2).plot(ax=axes[2], label = 'Obs.', linewidth = lw, color = 'k', linestyle = '--')
+    if "Obs." in dsq["runs"]:
+        dsq['Q'].sel(runs = 'Obs.', time = year_2).plot(ax=axes[2], label = 'Obs.', linewidth = lw, color = 'k', linestyle = '--')
         
     for ax in axes:
         ax.tick_params(axis = 'both', labelsize = fs)
@@ -252,7 +263,8 @@ def plot_hydro_1y(dsq, start_long, end_long, labels, colors, Folder_out, station
     #long period
     for label, color in zip(labels, colors):
         dsq['Q'].sel(runs = label, time = slice(start_long, end_long)).plot(ax=ax, label = label, linewidth = lw, color = color)
-    dsq['Q'].sel(runs = 'Obs.', time = slice(start_long, end_long)).plot(ax=ax, label = 'Obs.', linewidth = lw, color = 'k', linestyle = '--')
+    if "Obs." in dsq["runs"]:
+        dsq['Q'].sel(runs = 'Obs.', time = slice(start_long, end_long)).plot(ax=ax, label = 'Obs.', linewidth = lw, color = 'k', linestyle = '--')
         
 
     ax.tick_params(axis = 'both', labelsize = fs)
@@ -261,13 +273,13 @@ def plot_hydro_1y(dsq, start_long, end_long, labels, colors, Folder_out, station
     ax.set_title("")
     ax.legend(fontsize = fs)
     plt.tight_layout()
-    
+    fig.set_tight_layout(True)
     plt.savefig(os.path.join(Folder_out, f"hydro_{station_name}.png"), dpi=300)
     
 def plot_clim(ds_clim, Folder_out, station_name, lw = 0.8, fs = 8):
     fig, (ax1, ax2, ax3) = plt.subplots(3,1, figsize = (16/2.54, 15/2.54), sharex=True)
     #precip
-    P_sum_monthly = ds_clim['P'].resample(time = 'M').sum('time')
+    P_sum_monthly = ds_clim['P_subcatchment'].resample(time = 'M').sum('time')
     P_sum_monthly_mean = P_sum_monthly.groupby('time.month').mean('time')
     P_sum_monthly_q25 = P_sum_monthly.groupby('time.month').quantile(0.25, 'time')
     P_sum_monthly_q75 = P_sum_monthly.groupby('time.month').quantile(0.75, 'time')
@@ -277,7 +289,7 @@ def plot_clim(ds_clim, Folder_out, station_name, lw = 0.8, fs = 8):
     ax1.fill_between(np.arange(1,13), P_sum_monthly_q25, P_sum_monthly_q75, color = 'lightblue')
     
     #pot evap
-    P_sum_monthly = ds_clim['EP'].resample(time = 'M').sum('time')
+    P_sum_monthly = ds_clim['EP_subcatchment'].resample(time = 'M').sum('time')
     P_sum_monthly_mean = P_sum_monthly.groupby('time.month').mean('time')
     P_sum_monthly_q25 = P_sum_monthly.groupby('time.month').quantile(0.25, 'time')
     P_sum_monthly_q75 = P_sum_monthly.groupby('time.month').quantile(0.75, 'time')
@@ -288,9 +300,9 @@ def plot_clim(ds_clim, Folder_out, station_name, lw = 0.8, fs = 8):
 #    P_sum_monthly_mean.to_series().plot.bar(ax=ax1, color = 'lightblue')
     
     #temp
-    T_mean_monthly_mean = ds_clim['T'].groupby('time.month').mean('time')
-    T_mean_monthly_q25 = ds_clim['T'].groupby('time.month').quantile(0.25, 'time')
-    T_mean_monthly_q75 = ds_clim['T'].groupby('time.month').quantile(0.75, 'time')
+    T_mean_monthly_mean = ds_clim['T_subcatchment'].groupby('time.month').mean('time')
+    T_mean_monthly_q25 = ds_clim['T_subcatchment'].groupby('time.month').quantile(0.25, 'time')
+    T_mean_monthly_q75 = ds_clim['T_subcatchment'].groupby('time.month').quantile(0.75, 'time')
     #plot
     T_mean_monthly_mean.plot(ax=ax3, color = 'red')
     ax3.fill_between(np.arange(1,13), T_mean_monthly_q25, T_mean_monthly_q75, color = 'orange')
@@ -309,6 +321,7 @@ def plot_clim(ds_clim, Folder_out, station_name, lw = 0.8, fs = 8):
     ax3.set_xticks(ticks = np.arange(1,13), labels=month_labels, fontsize = fs)
     
     plt.tight_layout()
+    fig.set_tight_layout(True)
     plt.savefig(os.path.join(Folder_out, f"clim_{station_name}.png"), dpi=300)
     
         
