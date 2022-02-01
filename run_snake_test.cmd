a0f45221db00585cbee426fd41c1d4809108cb36
hboisgon <helene.boisgontier@deltares.nl>
2022-02-01 14:14:58 +0100

    Change folder names + first climate wk rule

--- run_snake_test.cmd
+++ run_snake_test.cmd
@@ -1,9 +1,16 @@
 call activate blueearth-cst
 
+rem Snakefile_model_creation
 snakemake -s Snakefile_model_creation --configfile config/snake_config_model_test.yml  --dag | dot -Tpng > dag_all.png
 
 snakemake --unlock -s Snakefile_model_creation --configfile config/snake_config_model_test.yml
 snakemake all -c 1 -s Snakefile_model_creation --configfile config/snake_config_model_test.yml
+
+rem Snakefile_climate_experiment
+snakemake -s Snakefile_climate_experiment --configfile config/snake_config_model_test.yml  --dag | dot -Tpng > dag_climate.png
+
+snakemake --unlock -s Snakefile_climate_experiment --configfile config/snake_config_model_test.yml
+snakemake all -c 1 -s Snakefile_climate_experiment --configfile config/snake_config_model_test.yml
 
 rem snakemake -s Snakefile_models all -c 1 --keep-going --until add_gauges --report --dryrun 
 rem keep going is when parallel runs to keep going parallel if one series goes wrong
---
+++ src/extract_historical_climate.py
@@ -0,0 +1,32 @@
+import os
+import geopandas as gpd
+import xarray as xr
+import hydromt
+
+from dask.diagnostics import ProgressBar
+
+region_fn = snakemake.input.prj_region
+fn_out = snakemake.output.climate_nc
+starttime = snakemake.params.starttime
+endtime = snakemake.params.endtime
+
+region = gpd.read_file(region_fn)
+
+source = 'era5_daily'
+
+data_catalog = hydromt.DataCatalog(deltares_data=True)
+print("Extracting historical climate grid")
+ds = data_catalog.get_rasterdataset(
+    source, 
+    geom = region,
+    time_tuple = (starttime, endtime),
+    buffer = 1,
+)
+
+dvars = ds.raster.vars
+encoding={k: {"zlib": True} for k in dvars}
+
+print("Saving to netcdf")
+delayed_obj = ds.to_netcdf(fn_out, encoding=encoding, mode="w", compute=False)
+with ProgressBar():
+    delayed_obj.compute()
