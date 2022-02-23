bd4164a7828ab8bd7ab40af76aeeb5e1bd0f9394
Umit Taner, Ph.D <tanerumit@gmail.com>
2022-02-23 08:15:35 +0100

    Delete reticulate_config.R

--- config/reticulate_config.R
+++
@@ -1,8 +0,0 @@
-
-
-
-
-# Specify the virtual python environment
-Sys.setenv(RETICULATE_PYTHON = "C:/Users/taner/Anaconda3/envs/blueearth-cst")
-reticulate::use_condaenv("C:/Users/taner/Anaconda3/envs/blueearth-cst", required = TRUE)
-#reticulate::py_config()
