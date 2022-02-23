431987d4aa04ff73c5f1d4984fad26f6e11c9e9e
hboisgon <helene.boisgontier@deltares.nl>
2022-02-23 09:00:19 +0100

    small bugfixes

--- src/weathergen/global.R
+++ src/weathergen/global.R
@@ -7,5 +7,5 @@ options(warn = -1) # Disable warnings
 #Sys.setenv(`_R_S3_METHOD_REGISTRATION_NOTE_OVERWRITES_` = "false")
 
 # Install and load libraries
-devtools::install_github("Deltares/weathergenr", ask = "never")
+devtools::install_github("Deltares/weathergenr") #, ask = "never")
 library(weathergenr)
