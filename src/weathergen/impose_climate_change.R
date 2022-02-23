0e0d858eea35b067b127ac4771779b3fdc4446d1
tanerumit <tanerumit@gmail.com>
2022-02-23 12:50:54 +0100

    add pet calculation logical flag for weather realizations

--- src/weathergen/impose_climate_change.R
+++ src/weathergen/impose_climate_change.R
@@ -1,62 +1,61 @@
 
+
+# GENERAL STRESS TEST PARAMETERS ###############################################
+
 # General R settings and prequisites
 source("./src/weathergen/global.R")
 
 # Read config
 yaml <- yaml::read_yaml(snakemake@params[["weagen_config"]])
+
+# General stress test parameters
 output_path <- snakemake@params[["output_path"]]
 nc_file_prefix <- snakemake@params[["nc_file_prefix"]]
-nc_file_suffix <- snakemake@params[["nc_file_suffix"]]
 
 # temp_change_type/precip_change_type [string]
 temp_change_type = yaml$temp$change_type
 precip_change_type = yaml$precip$change_type
 
 
-# Inputs varied within the loop ++++++++++++++++++++++++++++++++++++++++++++++
+# PARAMETERS CHANGING PER RUN ##################################################
 
-# stochastic_nc = Name of the gridded historical realization nc file [string]
+# Stochastic weather realization to be perturbed
 stochastic_nc <- snakemake@input[["rlz_nc"]]
+rlz_input <- weathergenr::readNetcdf(stochastic_nc)
+
+# scenario run identifier
+nc_file_suffix <- snakemake@params[["nc_file_suffix"]]
 
 # Climate stress file
 cst_csv_fn <- snakemake@input[["st_csv"]]
 cst_data <- read.csv(cst_csv_fn)
-
-# vector of monthly precip mean change factors [numeric vector with 12 values]
-current_precip_mean_change <- cst_data$temp_mean
-
-# strtest_matrix_precip_variance = vector of monthly precip variance change factors [numeric vector with 12 values]
-current_precip_variance_change <- cst_data$precip_mean
-
-# strtest_matrix_temp_mean = vector of monthly temperature mean changes, ad DegC [numeric vector with 12 values]
-current_temp_mean_change <- cst_data$precip_variance
-
-#rlz_input_name <- paste0(output_path, "/", stochastic_nc)
-rlz_input <- weathergenr::readNetcdf(stochastic_nc)
 
 # Apply climate changes to baseline weather data stored in the nc file
 rlz_future <- weathergenr::imposeClimateChanges(
    climate.data = rlz_input$data,
    climate.grid = rlz_input$grid,
    sim.dates = rlz_input$date,
-   change.factor.precip.mean = current_precip_mean_change,
-   change.factor.precip.variance = current_precip_variance_change,
-   change.factor.temp.mean = current_temp_mean_change,
+   change.factor.precip.mean = cst_data$precip_mean ,
+   change.factor.precip.variance = cst_data$precip_variance,
+   change.factor.temp.mean = cst_data$temp_mean,
    change.type.temp = temp_change_type,
-   change.type.precip = precip_change_type)
+   change.type.precip = precip_change_type,
+   calculate.pet = TRUE
+)
 
- # Save to netcdf file
- weathergenr::writeNetcdf(
-   data = rlz_future,
-   coord.grid = rlz_input$grid,
-   output.path = output_path,
-   origin.date =  rlz_input$date,
-   calendar.type = "noleap",
-   nc.template.file = stochastic_nc,
-   nc.compression = 4,
-   nc.spatial.ref = "spatial_ref",
-   nc.file.prefix = nc_file_prefix,
-   nc.file.suffix = nc_file_suffix)
+# Save to netcdf file
+weathergenr::writeNetcdf(
+ data = rlz_future,
+ coord.grid = rlz_input$grid,
+ output.path = output_path,
+ origin.date =  rlz_input$date,
+ calendar.type = "noleap",
+ nc.template.file = stochastic_nc,
+ nc.compression = 4,
+ nc.spatial.ref = "spatial_ref",
+ nc.file.prefix = nc_file_prefix,
+ nc.file.suffix = nc_file_suffix
+)
 
 
 ################################################################################
