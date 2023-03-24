690377a38e3d80fe0afc7f94955b143c8a3c6413
Ümit Taner <umit.Taner@deltares.nl>
2023-03-24 14:39:35 +0100

    clean up files

--- src/weathergen/install_rpackages.r
+++ src/weathergen/install_rpackages.r
@@ -2,4 +2,4 @@
 install.packages("rlang", repos = "http://cran.rstudio.com", dependencies = TRUE)
 
 # Install weathergenr package, but not don't update the dependencies
-devtools::install_github("Deltares/weathergenr@dev", upgrade = "never")
+devtools::install_github("Deltares/weathergenr", upgrade = "never")
