ec5cb5a00e3f553db7a462ae59f6a0243e17a21e
hboisgon <helene.boisgontier@deltares.nl>
2023-12-12 16:06:47 +0800

    update seaborn

--- src/plot_map.py
+++ src/plot_map.py
@@ -41,7 +41,7 @@ da.attrs.update(long_name="elevation", units="m")
 gdf_riv = mod.rivers
 # read/derive model basin boundary
 gdf_bas = mod.basins
-plt.style.use("seaborn-whitegrid")  # set nice style
+plt.style.use("whitegrid")  # set nice style
 # we assume the model maps are in the geographic CRS EPSG:4326
 proj = ccrs.PlateCarree()
 # adjust zoomlevel and figure size to your basis size & aspect
