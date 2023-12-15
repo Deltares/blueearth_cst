887ae06fe66a0feaca5b1c1197920f845f13c0d4
Jaap <33715902+Jaapel@users.noreply.github.com>
2023-12-15 13:57:31 +0100

    actual merge commit

--- src/plot_results.py
+++ src/plot_results.py
@@ -5,25 +5,24 @@ Created on Wed Jul 14 09:18:38 2021
 @author: bouaziz
 """
 
-import xarray as xr
-import numpy as np
 import os
 from os.path import join
 from pathlib import Path
-import matplotlib.pyplot as plt
-import pandas as pd
-import hydromt
-from hydromt_wflow import WflowModel
-
 from typing import Union
 
+import hydromt
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import xarray as xr
 from func_plot_signature import (
-    plot_signatures,
+    plot_basavg,
+    plot_clim,
     plot_hydro,
     plot_hydro_1y,
-    plot_clim,
-    plot_basavg,
+    plot_signatures,
 )
+from hydromt_wflow import WflowModel
 
 
 def analyse_wflow_historical(
@@ -177,127 +176,6 @@ def analyse_wflow_historical(
     else:
         ds_list = [ds_sim_gauges, ds_outlocs]
 
-<<<<<<< HEAD
-# Other wflow outputs for wflow basin
-ds_basin = xr.merge([mod.results[dvar] for dvar in mod.results if "_basavg" in dvar])
-ds_basin = ds_basin.squeeze(drop=True)
-# If precipitation, skip as this will be plotted with the other climate data
-if "precipitation_basavg" in ds_basin:
-    ds_basin = ds_basin.drop_vars("precipitation_basavg")
-
-# %% Read the observations data
-# read timeseries data and match with existing gdf
-has_observations = False
-if observations_timeseries is not None:
-    if os.path.exists(observations_timeseries):
-        has_observations = True
-
-# Discharge data
-# make sure the user provided a observation file and ouput locations
-if (f"gauges_{gauges_output_name}" in mod.staticgeoms) & (has_observations):
-    name = f"gauges_{gauges_output_name}"  # gauges locations in staticgeoms
-    da_ts = hydromt.io.open_timeseries_from_table(
-        observations_timeseries, name=name, sep=";"
-    )
-    # HydroMT bug to be fixed index len and names on gdf_outltocs and da_ts need to be an excact match
-    # Find common index in gdf_outlocs and da_ts and keep only the common ones
-    common_index = gdf_outlocs.index.intersection(da_ts.index)
-    gdf_outlocs = gdf_outlocs.loc[common_index]
-    da_ts = da_ts.sel(index=common_index)
-    da = hydromt.vector.GeoDataArray.from_gdf(gdf_outlocs, da_ts, index_dim="index")
-    qobs_outloc = da
-    # rename run to Obs. and rename var to Q
-    ds_obs = (
-        qobs_outloc.to_dataset()
-        .assign_coords({"runs": "Obs."})
-        .expand_dims("runs")
-        .rename({f"gauges_{gauges_output_name }": "Q"})
-    )
-
-# %% make plots - first loop over output locations
-
-# combine sim and obs at outputloc in one dataset if timeseries observations exist
-if (f"gauges_{gauges_output_name}" in mod.staticgeoms) & (has_observations):
-    ds_outlocs = ds_obs.combine_first(ds_sim_outlocs)
-    # combine_first now seems to somehow drop the coordinate station_name?
-    ds_outlocs["station_name"] = ds_obs["station_name"]
-    ds_outlocs = ds_outlocs.set_coords("station_name")
-
-# rename Q in modeled dataset at gauges locations
-ds_sim_gauges = (
-    qsim_gauges.to_dataset()
-    .assign_coords({"runs": labels[0]})
-    .expand_dims("runs")
-    .rename({"Q_gauges": "Q"})
-)
-
-# select dataset based on gauges or/and outputloc locations
-# if no user output and observations are provided:
-if ((f"{gauges_output_name}" in mod.staticgeoms) == False) & (not has_observations):
-    ds_list = [ds_sim_gauges]
-# if user output locs are available but no observations timeseries:
-elif ((f"{gauges_output_name}" in mod.staticgeoms) == True) & (not has_observations):
-    ds_list = [ds_sim_gauges, ds_sim_outlocs]
-# if output locs and observations are available - make hydro plots for gauges and make hydro and signature plots for outputlocs
-else:
-    ds_list = [ds_sim_gauges, ds_outlocs]
-
-# plot and loop over datasets with outlocs and gauges locations
-df_perf_all = pd.DataFrame()
-for ds in ds_list:
-    for station_id, station_name in zip(ds.index.values, ds.station_name.values):
-        print(station_id, station_name)
-        # skip first year for hydro -- warm up period
-        if len(ds.time) > 365:
-            dsq = ds.sel(index=station_id).sel(
-                time=slice(
-                    f"{ds['time.year'][0].values+1}-{ds['time.month'][0].values}-{ds['time.day'][0].values}",
-                    None,
-                )
-            )  # .dropna(dim='time')
-        else:
-            dsq = ds.sel(index=station_id)
-        # plot hydro
-        if len(np.unique(dsq["time.year"])) >= 3:
-            year_min = pd.to_datetime(dsq["Q"].sel(runs="Mod.").idxmin().values).year
-            year_max = pd.to_datetime(dsq["Q"].sel(runs="Mod.").idxmax().values).year
-            # if min and max occur during the same year, select 2nd year with modeled min flow:
-            if year_min == year_max:
-                year_min = (
-                    dsq.resample(time="A").min("time").isel(time=1)["time.year"].values
-                )
-            plot_hydro(
-                dsq,
-                dsq.time[0],
-                dsq.time[-1],
-                str(year_max),
-                str(year_min),
-                labels,
-                colors,
-                Folder_plots,
-                station_name,
-            )
-            plt.close()
-        else:
-            plot_hydro_1y(
-                dsq,
-                dsq.time[0],
-                dsq.time[-1],
-                labels,
-                colors,
-                Folder_plots,
-                station_name,
-            )
-            plt.close()
-
-        # dropna time for signature calculations.
-        # skip first year for signatures -- warm up period -- if model did not run for a full year - skip signature plots
-        if len(ds.time) > 365:
-            dsq = (
-                ds["Q"]
-                .sel(index=station_id)
-                .sel(
-=======
     # plot and loop over datasets with outlocs and gauges locations
     df_perf_all = pd.DataFrame()
     for ds in ds_list:
@@ -306,7 +184,6 @@ for ds in ds_list:
             # skip first year for hydro -- warm up period
             if len(ds.time) > 365:
                 dsq = ds.sel(index=station_id).sel(
->>>>>>> main
                     time=slice(
                         f"{ds['time.year'][0].values+1}-{ds['time.month'][0].values}-{ds['time.day'][0].values}",
                         None,
