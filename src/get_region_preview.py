0338dad4c43cbc6aad3e3e6c25542a53788faf4b
Tjalling-dejong <tjalling.dejong@deltares.nl>
2024-02-20 10:54:21 +0100

    finished get_region_preview.py

--- src/get_region_preview.py
+++ src/get_region_preview.py
@@ -1,7 +1,26 @@
+""" A command line script for delineating basins and subbasin with river geometries.
+    The script expects a region string in the following format:
+    -r "{'basin': [x,y]}"
+
+    A data catalog file path:
+    -d path/to/datacatalog
+
+    A path to an output directory for the region GeoJSON
+    -p path/to/output/dir
+
+    There is also the option to use ifferent hydrography and river files:
+    -h <hydrography file name>, by default merit_hydro_ihu
+    -n <rivers file name>, by default rivers_atlas_v10
+
+    The resulting (sub)basin is written as a GeoJSON to the given output directory
+    in a folder named region    
+"""
+
 from json import loads
 import argparse
 import logging
 from typing import List
+import os
 
 import hydromt
 from hydromt.cli.api import get_region
@@ -52,7 +71,8 @@ if __name__ == "__main__":
         "--region",
         help="Geometry of interest for which a basin/subbasin needs to be delineated.",
     )
-    parser.add_argument("-d", "--datacatalog", help="path to data catalog")
+    parser.add_argument("-d", "--datacatalog", help="Path to data catalog")
+    parser.add_argument("-p", "--path", help="Path to save the region geojson file to")
     parser.add_argument(
         "-f",
         "--hydrography_fn",
@@ -70,14 +90,20 @@ if __name__ == "__main__":
     args = parser.parse_args()
     region_json = args.region.replace("'", '"')
     region = loads(region_json)
-    print(region == dict)
-    region_geom = get_basin_preview(region=args.region, datacatalog_fn=args.datacatalog)
+    region_geom = get_basin_preview(
+        region=region,
+        datacatalog_fn=args.datacatalog,
+        hydrography_fn=args.hydrography_fn,
+    )
     river_geom = get_river_preview(
-        region_geojson=region_geom,
+        region=region_geom,
         data_catalog_fn=args.datacatalog,
-        region=args.rivers_fn,
+        rivers_fn=args.rivers_fn,
     )
 
     region = gpd.GeoDataFrame(pd.concat([region_geom, river_geom]))
-    region_geojson = region.to_json()
-    print(region_geojson)
+    file_dir = os.path.join(args.path, "region")
+    if not os.path.isdir(file_dir):
+        os.mkdir(file_dir)
+    file_path = os.path.join(file_dir, "region.geojson")
+    region_geojson = region.to_file(filename=file_path, driver="GeoJSON")
