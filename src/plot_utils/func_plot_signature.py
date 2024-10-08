correct units of historical hydro plots

src/plot_utils/func_plot_signature.py
@@ -737,7 +737,7 @@ def plot_hydro(
         titles = [
             "Daily time-series",
             "Annual time-series",
-            "Annual cycle",
+            "Monthly regime",
             "Wettest year",
             "Driest year",
         ]
@@ -775,22 +775,23 @@ def plot_hydro(
     if nb_panel == 5:
         # 2. annual Q
         for climate_source in qsim.climate_source.values:
-            qsim.sel(climate_source=climate_source).resample(time="YE").sum(
-                skipna=True, min_count=min_count_year
-            ).plot(
+            qsim.sel(climate_source=climate_source).resample(time="YE").mean().plot(
                 ax=axes[1],
                 label=f"simulated {climate_source}",
                 linewidth=lw,
                 color=color[climate_source],
             )
         if qobs is not None:
-            qobs.resample(time="YE").sum(skipna=True, min_count=min_count_year).plot(
+            qobs_year = qobs.resample(time="YE").sum(
+                skipna=True, min_count=min_count_year
+            ) / qobs.resample(time="YE").count(dim="time")
+            qobs_year.plot(
                 ax=axes[1], label=labobs, linewidth=lw, color=colobs, linestyle="--"
             )
 
         # 3. monthly Q
         month_labels = ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"]
-        dsqM = qsim_na.resample(time="ME").sum()
+        dsqM = qsim_na.resample(time="ME").mean()
         dsqM = dsqM.groupby(dsqM.time.dt.month).mean()
         for climate_source in qsim.climate_source.values:
             dsqM.sel(climate_source=climate_source).plot(
@@ -802,10 +803,9 @@ def plot_hydro(
         if qobs is not None:
             dsqMo = qobs_na.resample(time="ME").sum(
                 skipna=True, min_count=min_count_month
-            )
+            ) / qobs_na.resample(time="ME").count(dim="time")
             dsqMo = dsqMo.groupby(dsqMo.time.dt.month).mean()
             dsqMo.plot(ax=axes[2], label=labobs, linewidth=lw, color=colobs)
-        axes[2].set_title("Average monthly sum")
         axes[2].set_xticks(ticks=np.arange(1, 13), labels=month_labels, fontsize=5)
 
         # 4. wettest year
@@ -837,13 +837,7 @@ def plot_hydro(
     # Axes settings
     for ax, title in zip(axes, titles):
         ax.tick_params(axis="both", labelsize=fs)
-        if ax == axes[0]:
-            ax.set_ylabel("Q (m$^3$s$^{-1}$)", fontsize=fs)
-        elif nb_panel == 5:
-            if ax == axes[1]:
-                ax.set_ylabel("Q (m$^3$yr$^{-1}$)", fontsize=fs)
-            elif ax == axes[2]:
-                ax.set_ylabel("Q (m$^3$month$^{-1}$)", fontsize=fs)
+        ax.set_ylabel("Q (m$^3$s$^{-1}$)", fontsize=fs)
         ax.set_title(title, fontsize=fs)
         ax.set_xlabel("", fontsize=fs)
     axes[0].legend(fontsize=fs)